2ce52438054a686fad614e561ba7bce4fec453d1
Damian Chojnacki <damianchojnacki@op.pl>
2020-02-15 16:12:04 +0100

    Update Progress.jsx

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -14,7 +14,7 @@ function Progress() {
         for(let i = 1; i <= state.progress.max; i++){
             const Icon = Icons[state.monsters[i - 1].icon];
 
-            marks.push(<div className="progress__mark z-20 text-2xl text-white bg-gray-500" key={i} style={{left: i / state.progress.max * 100 + "%"}}><Icon/></div>);
+            marks.push(<div className="progress__mark z-20 text-2xl text-white bg-gray-500 border-solid border-2 border-gray-600" key={i} style={{left: i / state.progress.max * 100 + "%"}}><Icon/></div>);
 
             console.log(marks);
         }
@@ -26,7 +26,7 @@ function Progress() {
         <div className="hidden md:block absolute w-full bottom-0">
             <div className="absolute w-1/12 bg-gray-500 py-1 bottom-0"/>
             <div className="w-11/12 relative ml-8">
-                <span className="progress__dot bg-gray-700" style={{left: state.progress.percent + "%"}}/>
+                <span className="progress__dot bg-gray-700 shadow" style={{left: state.progress.percent + "%"}}/>
                 <div className="progress z-10">
                     {createMarks()}
                 </div>
