8efc7e1e7ec4ffb3b8eee95209e49c671e7da8d1
Damian Chojnacki <daamian3@gmail.com>
2020-02-25 12:08:57 +0100

    Update Rules.jsx

--- src/components/Rules.jsx
+++ src/components/Rules.jsx
@@ -32,16 +32,14 @@ function Rules(){
                             <li>po lewej stronie możesz zauważyć nazwę i ikonę bohatera</li>
                             <li>po prawej stronie znajdują się jego statystyki, które zwiększają się wraz z poziomem.</li>
                         </ul>
-                        Wraz z pokonywaniem przeciwników zdobywasz złoto, które można przeznaczyć na awans bohatera. Gdy zdobędziesz wystarczającą ilość złota, wyświetli się okno ulepszenia.
+                        Wraz z pokonywaniem przeciwników zdobywasz złoto, które można przeznaczyć na awans bohatera. Gdy zdobędziesz wystarczającą ilość złota, wyświetli się okno. Po walce będziesz musiał uleczyć bohatera, jeśli chcesz aby walczył w pełni sił.
                     </li>
                     <li className="my-4">
                         Panel walki
                         <ul className="list-disc ml-5">
                             <li>po lewej stronie zobaczysz przeciwnika, z którym musisz się zmierzyć</li>
                             <li>za pomocą przycisków po prawej stronie wybierasz bohatera, który zaatakuje przeciwnika</li>
-                            <li>UWAGA - na turę po zadaniu obrażeń, bohater jest wystawiony na atak (obrona zmniejsza się do 0)</li>
                         </ul>
-                        Po zadaniu 4 ciosów w pierwszej turze, przeciwnik zaatakuje losowego bohatera. W kolejnych turach masz jedynie 3 ciosy zanim zostaniesz zaatakowany.
                     </li>
                 </ol>
             </div>
